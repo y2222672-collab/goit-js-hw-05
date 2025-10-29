7046484284d1e014fddbe24737679acdd330fbcd
Valeria_Stratulat <y2222672@gmail.com>
2025-10-29 20:34:42 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+  const numArray = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      numArray.push(numbers[i]);
+    }
+  }
+  return numArray;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
